de95c32940c393d9873e950298ec410994f67685
Minh Anh Nguyen <minhanhn@usf.edu>
2025-10-26 15:56:38 -0400

    import counters to home page and set default and custom counters

--- src/app/Counter.jsx
+++ src/app/Counter.jsx
@@ -4,20 +4,21 @@ export default function Counter({initialCount=0, initialStep=1}) {
     const [count, setCount] = useState(initialCount);
     const [step, setStep] = useState(initialStep);
 
+ 
     return (
         <div>
-            <p>Current Sales Count: {count}</p>
-            <button id="add-button" onClick={() => setCount(count + 1)}>Add Sale</button><br />
+            <p aria-live="polite">Current Sales Count: {count}</p>
+            <button id="add-button" onClick={() => setCount(count + step)}>Add Sale</button><br />
             
-            <button id="delete-button" onClick={() => setCount(count - 1)}>Delete Sale</button><br />
+            <button id="delete-button" onClick={() => setCount(count - step)} disabled={count-step <0}>Delete Sale</button><br />
             
             <button id="reset-button" onClick={() => setCount(0)}>Reset Sale</button><br />
            
-            <label> Step
-                <input type="number"
-                value={step}
-                onChange={(e) => setStep(Number(e.target.value))} />
+            <label> 
+                Step: 
+                <input type="number" value={step} onChange={(e) => setStep(Number(e.target.value))} />
             </label>
         </div>
     );
+    
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,13 @@ import Counter from "./Counter";
 export default function Home() {
   return (
     <main>
-      
-      <h1> Counter Section </h1>
+      <h1>Sales Counter</h1>
+
+      <h2>Default Counter</h2>
       <Counter />
+
+      <h2>Custom Counter</h2>
+      <Counter initialCount={5} initialStep={2} />
     </main>
   )
 }
